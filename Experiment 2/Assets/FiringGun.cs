4c4cf330761f12a4ac3b9b71004e2e93dbea725b
Marc Brooks <marc.brooks@students.plymouth.ac.uk>
2017-08-15 03:56:30 +0100

    Experiment 2 part 3 of 3

---
+++ Experiment 2/Assets/FiringGun.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FiringGun : MonoBehaviour
+{
+    public float damage = 10f;
+    public float range = 100f;
+    public Camera cam;
+    public ParticleSystem muzzleFlash;
+    public GameObject impactEffect;
+    public float impactForce = 30f;
+    public float fireRate = 15f;
+    public bool ve;
+    private Valve.VR.EVRButtonId triggerButton = Valve.VR.EVRButtonId.k_EButton_SteamVR_Trigger;
+    private SteamVR_Controller.Device controller { get { return SteamVR_Controller.Input((int)trackedObj.index); } }
+    private SteamVR_TrackedObject trackedObj;
+
+    private float nextTimeToFire = 0f;
+
+	// Use this for initialization
+	void Start ()
+    {
+        trackedObj = GetComponent<SteamVR_TrackedObject>();
+	}	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (!ve)
+        {
+            if (Input.GetButtonDown("Fire1")) // && Time.time >= nextTimeToFire)
+            {
+             //   nextTimeToFire = Time.time + 1f / fireRate;
+                Shoot();
+            }
+        }
+        if (ve)
+        {
+            if (controller.GetPressDown(triggerButton))
+            {
+                //nextTimeToFire = Time.time + 1f / fireRate;
+                Shoot();
+            }
+        }
+    }
+    void Shoot()
+    {
+        muzzleFlash.Play();
+        RaycastHit hit;
+        if (!ve)
+        {
+            if (Physics.Raycast(cam.transform.position, cam.transform.forward, out hit, range))
+            {
+                Debug.Log(hit.transform.name);
+
+
+                Target target = hit.transform.GetComponent<Target>();
+                if (target != null)
+                {
+                    target.TakeDamage(damage);
+                }
+                if (hit.rigidbody != null)
+                {
+                    hit.rigidbody.AddForce(-hit.normal * impactForce);
+                }
+                GameObject impactGo = Instantiate(impactEffect, hit.point, Quaternion.LookRotation(hit.normal));
+                Destroy(impactGo, 2f);
+            }
+        }
+        if (ve)
+        {
+            if (Physics.Raycast(transform.position, transform.forward, out hit, range))
+            {
+                Debug.Log(hit.transform.name);
+                Target target = hit.transform.GetComponent<Target>();
+                if (target != null)
+                {
+                    target.TakeDamage(damage);
+                }
+                if (hit.rigidbody != null)
+                {
+                    hit.rigidbody.AddForce(-hit.normal * impactForce);
+                }
+                GameObject impactGo = Instantiate(impactEffect, hit.point, Quaternion.LookRotation(hit.normal));
+                Destroy(impactGo, 2f);
+            }
+        }
+    }
+}
